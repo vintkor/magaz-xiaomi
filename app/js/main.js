0aae4848266370b882659d075b30832ed74deea2
vintkor <alkv84@yandex.ru>
2016-08-15 14:04:06 +0300

    Rename some files

--- app/js/main.js
+++ app/js/main.js
@@ -1,95 +1,100 @@
- 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-	
-	/*-------------------------------- Плавная анимация по меню ---------------------------------*/
 
-	$(".bounce").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+    /*-------------------------------- Плавная анимация по меню ---------------------------------*/
 
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
+    $(".bounce").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
 
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 800);
-	});
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
 
-	/*------------------------------- Получение JSON с отправленных данных -----------------------------*/
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
 
-	$(".ajaxform").submit(function(){
-		var form = $(this);
-		var error = false;
-		if (!error) {
-			var data = form.serialize();
-			function success(returneData) {
-				console.log(returneData);
-			}
-			$.ajax({
-				type: 'POST',
-				url: 'mailto.php',
-				dataType: 'json',
-				data: data,
-				success: function(data) {
-					var model = data.value;
-					$('#hidden-jquery').val(model);
-				}
-			});
-		}
-		return false;
-	});
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 800);
+    });
 
-	$(function(){
-	    $('#Container').mixItUp();
-	});
+    /*------------------------------- Получение JSON с отправленных данных -----------------------------*/
 
-	/*------------------------------- Button to TOP -----------------------------*/
+    $(".ajaxform").submit(function() {
+        var form = $(this);
+        var error = false;
+        if (!error) {
+            var data = form.serialize();
 
-	$(function() { 
-	  $(window).scroll(function() {   
-	    if($(this).scrollTop() != 0) {     
-	      $('#toTop').fadeIn();     
-	    } else {     
-	      $('#toTop').fadeOut();   
-	    }   
-	  });   
-	  $('#toTop').click(function() {   
-	    $('body,html').animate({scrollTop:0},800);   
-	  }); 
-	});   
+            function success(returneData) {
+                console.log(returneData);
+            }
+            $.ajax({
+                type: 'POST',
+                url: 'json.php',
+                dataType: 'json',
+                data: data,
+                success: function(data) {
+                    var model = data.value;
+                    $('#hidden-jquery').val(model);
+                }
+            });
+        }
+        return false;
+    });
 
-	/*------------------------------- Маска для поля номера телефона -----------------------------*/
-	jQuery(function($){
-	    $("#number").mask("8 (999) 999-99-99");
-	});
+    $(function() {
+        $('#Container').mixItUp();
+    });
+
+    /*------------------------------- Button to TOP -----------------------------*/
+
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() != 0) {
+                $('#toTop').fadeIn();
+            } else {
+                $('#toTop').fadeOut();
+            }
+        });
+        $('#toTop').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+        });
+    });
+
+    /*------------------------------- Маска для поля номера телефона -----------------------------*/
+
+    jQuery(function($) {
+        $("#number").mask("8 (999) 999-99-99");
+    });
 
 
-	/*------------------------------- Owl.Carousel -----------------------------*/
+    /*------------------------------- Owl.Carousel -----------------------------*/
 
-	$(".owl-carousel").owlCarousel({
-		loop:true,
-		margin:20,
-		lazyLoad:true,
-		autoplay:true,
-	    autoplayTimeout:2000,
-	    autoplayHoverPause:true,
-	    nav:true,
-	    navText: [ '&larr;', '&rarr;' ],
-        responsive:{
-            0:{
-                items:1
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        lazyLoad: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ['&larr;', '&rarr;'],
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
-	});
+    });
 
 });
--- app/json.php
+++ app/json.php
@@ -1,14 +1,8 @@
-<?
-
+<?php
 
 	if(isset($_POST['model'])){
 		$test = $_POST['model'];
-		$json = '{
-			"value": "'
-			. $test
-			. '"
-		}';
+		$json = '{"value": "' . $test	. '"}';
 	}
-
 
 	echo $json;
